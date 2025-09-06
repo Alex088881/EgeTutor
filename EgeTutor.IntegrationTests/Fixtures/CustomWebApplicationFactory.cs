1e6efbf4b8906bbea3d96cc7f196b8655c8b3eee
alex <alex221292@mail.ru>
2025-09-06 12:36:08 +0500

    Создали фабрику для тестового приложения

--- EgeTutor.IntegrationTests/Fixtures/CustomWebApplicationFactory.cs
+++ EgeTutor.IntegrationTests/Fixtures/CustomWebApplicationFactory.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using EgeTutor.API;
+using EgeTutor.Persistence.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EgeTutor.IntegrationTests.Fixtures
 {
-    internal class CustomWebApplicationFactory
+    public class CustomWebApplicationFactory:WebApplicationFactory<Program>
     {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services => {
+                // Удаляем регистрацию реального DbContext
+                var descroptor = services.SingleOrDefault(d=>d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+                if (descroptor != null)
+                {
+                    services.Remove(descroptor);
+                }
+                // Добавляем InMemory базу данных для тестов
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TestDatabase");
+                });
+
+                // Создаём scope для получения сервисов
+                var serviceProvider = services.BuildServiceProvider();
+                using var scope = serviceProvider.CreateScope();
+                var scopedSevices = scope.ServiceProvider;
+                var db = scopedSevices.GetRequiredService<ApplicationDbContext>();
+
+                // Гарантируем, что база данных создана
+                db.Database.EnsureCreated();
+            });
+        }
     }
 }
